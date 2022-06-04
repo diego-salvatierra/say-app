62461217676b0480f7debf685e0cdba36e799624
Diego Salvatierra <diegosalva.ds@gmail.com>
2022-06-04 11:45:47 -0400

    organized folders

--- App/index.js
+++ App/index.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button } from '@rneui/base';
+
+const AwesomeButton = () => (<Button title='Welcome'/>)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!</Text>
       <StatusBar style="auto" />
+      <AwesomeButton></AwesomeButton>
     </View>
   );
 }
