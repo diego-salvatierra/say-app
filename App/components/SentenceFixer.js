b6112bdad511aaf448a4943c7a7a783eea75e01e
Diego Salvatierra <diegosalva.ds@gmail.com>
2022-11-18 15:36:37 -0800

    fixed decode bug

--- App/components/SentenceFixer.js
+++ App/components/SentenceFixer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from "react-native";
+import {decode} from 'html-entities'
 
 // SHOULD ONLY RUN WHEN READY
 
@@ -53,7 +54,7 @@ const SentenceFixer = ({ sentence }) => {
           .then((response) => {
               console.log("response from google GO: ", response.data.translations[0].translatedText);
               console.log("intermediateSent INSIDE 1 is ", intermediateSent)
-              setIntermediateSent(response.data.translations[0].translatedText) // set to intermediate sentence state
+              setIntermediateSent(decode(response.data.translations[0].translatedText)) // set to intermediate sentence state
               console.log("intermediateSent INSIDE 2 is ", intermediateSent)
             })
           .catch(error => {
@@ -86,7 +87,7 @@ const SentenceFixer = ({ sentence }) => {
           .then(res => res.json())
           .then((response) => {
               console.log("response from google BACK: ", response.data.translations[0].translatedText);
-              setFixedSentence(response.data.translations[0].translatedText)
+              setFixedSentence(decode(response.data.translations[0].translatedText))
             })
           .catch(error => {
               console.log("There was an error with the translation request: ", error);
@@ -98,47 +99,6 @@ const SentenceFixer = ({ sentence }) => {
     fixSentence()
     },
     [fixedSentence, intermediateSent, callUrl])
-
-    /*
-
-    fetch('https://mywebsite.com/endpoint/', {
-        method: 'POST',
-        headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        firstParam: 'yourValue',
-        secondParam: 'yourOtherValue'
-    })
-    }); */
-
-    /* 
-    const projectId = 'AIzaSyAJ1-ryN8FFi1IqqUI9QZ4PSGO9MiUOkY4';
-
-    // Imports the Google Cloud client library
-    const {Translate} = require('@google-cloud/translate').v2;
-
-    // Instantiates a client
-    const translate = new Translate({projectId});
-
-    async function quickStart() {
-        // The text to translate
-        const text = 'Hello, world!';
-
-        // The target language
-        const target = 'es';
-
-        // Translates some text into Russian
-        const [translation] = await translate.translate(text, target);
-        console.log(`Text: ${text}`);
-        console.log(`Translation: ${translation}`);
-    }
-
-    quickStart();
-
-    console.log("sentence is ", sentence) */
-
 
     return (
         <View>
